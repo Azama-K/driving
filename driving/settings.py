f5b6002cf7ca4379b3dd3e4d2e7a2fc86dbda75e
AzamA38 <e185731@ie.u-ryukyu.ac.jp>
2020-12-04 15:14:44 +0900

    SECRET_KEY変更

--- driving/settings.py
+++ driving/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm_9wxz%z770jbi87z^nmgl_&n*ujm0$p8j#dofe9l-e+&kk%4y'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
